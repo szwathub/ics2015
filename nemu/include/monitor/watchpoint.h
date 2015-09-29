73bd1109cab3e46d82ef0c5fb68fa82c4b46171c
CyanMac <U201315021@hust.edu.cn>
2015-09-29 11:04:15 +0800

    Add Reg-Print

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,12 +6,13 @@
 typedef struct watchpoint {
 	int NO;
 	uint32_t watchpointadd;
-	uint32_t date;
+	//uint32_t date;
 	int exist;
 	struct watchpoint *next;
 
 	/* TODO: Add more members if necessary */
 } WP;
 
-WP** get_wp_head();
+WP* get_wp_head();
+
 #endif
