6305b6f74f577f9a125f7f728760cc93bb7ea6b5
szwathub <szwathub@gmail.com>
2015-10-07 13:13:51 +0800

    add more roles

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -7,7 +7,7 @@
 #include <regex.h>
 
 enum {
-	NOTYPE = 256, EQ
+	NOTYPE = 256, EQ, NUM
 
 	/* TODO: Add more token types */
 
@@ -24,7 +24,13 @@ static struct rule {
 
 	{" +",	NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
-	{"==", EQ}						// equal
+	{"==", EQ},						// equal
+	{"\\-", '-'},					//sub
+	{"\\*", '*'},					//mul
+	{"\\/", '/'},					//div
+	{"\\(", '('},
+	{"\\)", ')'},
+	{"\\/:", NUM}					//number
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -43,7 +49,8 @@ void init_regex() {
 		ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
 		if(ret != 0) {
 			regerror(ret, &re[i], error_msg, 128);
-			Assert(ret == 0, "regex compilation failed: %s\n%s", error_msg, rules[i].regex);
+			Assert(ret == 0, "regex compilation failed: %s\n%s",
+					error_msg, rules[i].regex);
 		}
 	}
 }
@@ -60,25 +67,31 @@ static bool make_token(char *e) {
 	int position = 0;
 	int i;
 	regmatch_t pmatch;
-	
+
 	nr_token = 0;
 
 	while(e[position] != '\0') {
 		/* Try all rules one by one. */
 		for(i = 0; i < NR_REGEX; i ++) {
-			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0
+				&& pmatch.rm_so == 0) {
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
-				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+
+				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+					i, rules[i].regex, position, substr_len,
+					substr_len, substr_start);
+
 				position += substr_len;
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes
-				 * to record the token in the array ``tokens''. For certain 
+				 * to record the token in the array ``tokens''. For certain
 				 * types of tokens, some extra actions should be performed.
 				 */
 
 				switch(rules[i].token_type) {
+					case '+': printf("Ok");
 					default: panic("please implement me");
 				}
 
@@ -87,22 +100,53 @@ static bool make_token(char *e) {
 		}
 
 		if(i == NR_REGEX) {
-			printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+			printf("no match at position %d\n%s\n%*.s^\n",
+				position, e, position, "");
 			return false;
 		}
 	}
 
-	return true; 
+	return true;
 }
 
+/*
+ * @describe evaluate the expression and return the value
+ * @param {int, int} p, q
+ * @return {int} 
+ */
+int eval(int p, int q) {
+	if(p > q) {
+		/* Bad expression */
+	}
+	else if(p == q) {
+		/* 
+		 * Single token
+		 * For now, this token should be a number
+		 * return the value of the exprssion
+		 */
+	}
+	else {
+		val1 = eval(p, op-1);
+		val2 = eval(op+1, q);
+
+		switch(token_type) {
+			case '+': return val1 + val2;
+			case '-': return /* */
+			case '*': ;
+			case '/': ;
+			default: assert(0);
+		}
+	}
+}
+				
+			
 uint32_t expr(char *e, bool *success) {
 	if(!make_token(e)) {
 		*success = false;
 		return 0;
-	}
+}
 
 	/* TODO: Insert codes to evaluate the expression. */
 	panic("please implement me");
 	return 0;
 }
-
