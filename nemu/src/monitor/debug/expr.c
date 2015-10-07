5f08b2c2187556ea3b439c4657fb11e0e0dc4230
szwathub <szwathub@gmail.com>
2015-10-07 19:39:15 +0800

    parse regex

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,8 +6,10 @@
 #include <sys/types.h>
 #include <regex.h>
 
+char *substring(char* ch, int pos, int length);
+
 enum {
-	NOTYPE = 256, EQ, NUM
+	NOTYPE = 256, EQ, HEX_NUM, DEC_NUM, REG
 
 	/* TODO: Add more token types */
 
@@ -25,12 +27,14 @@ static struct rule {
 	{" +",	NOTYPE},				// spaces
 	{"\\+", '+'},					// plus
 	{"==", EQ},						// equal
-	{"\\-", '-'},					//sub
+	{"-", '-'},						//sub
 	{"\\*", '*'},					//mul
-	{"\\/", '/'},					//div
+	{"/", '/'},						//div
 	{"\\(", '('},
 	{"\\)", ')'},
-	{"\\/:", NUM}					//number
+	{"[0-9]+", DEC_NUM},			//number
+	{"0x[0-9a-fA-f]+", HEX_NUM},	//
+	{"\\$[a-z]+", REG},				//REG
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
@@ -75,6 +79,7 @@ static bool make_token(char *e) {
 		for(i = 0; i < NR_REGEX; i ++) {
 			if(regexec(&re[i], e + position, 1, &pmatch, 0) == 0
 				&& pmatch.rm_so == 0) {
+				nr_token++;
 				char *substr_start = e + position;
 				int substr_len = pmatch.rm_eo;
 
@@ -91,8 +96,54 @@ static bool make_token(char *e) {
 				 */
 
 				switch(rules[i].token_type) {
-					case '+': printf("Ok");
-					default: panic("please implement me");
+					case DEC_NUM:
+						tokens[nr_token-1].type = DEC_NUM;
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case HEX_NUM:
+						tokens[nr_token].type = HEX_NUM;
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case '+':
+						tokens[nr_token-1].type = '+';
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case '-':
+						tokens[nr_token].type = '-';
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case '*':
+						tokens[nr_token].type = '*';
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case '/':
+						tokens[nr_token].type = '/';
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case '(':
+						tokens[nr_token].type = '(';
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+								break;
+					case ')':
+						tokens[nr_token].type = ')';
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case REG:
+						tokens[nr_token].type = REG;
+						strcpy(tokens[nr_token-1].str,
+								substring(substr_start, 0, substr_len));
+						break;
+					case NOTYPE:
+						break;
+					//default: panic("please implement me");
 				}
 
 				break;
@@ -112,41 +163,79 @@ static bool make_token(char *e) {
 /*
  * @describe evaluate the expression and return the value
  * @param {int, int} p, q
- * @return {int} 
+ * @return {int} value
  */
+
 int eval(int p, int q) {
 	if(p > q) {
 		/* Bad expression */
+		;
 	}
 	else if(p == q) {
-		/* 
+		/*
 		 * Single token
 		 * For now, this token should be a number
 		 * return the value of the exprssion
 		 */
+		 ;
 	}
 	else {
-		val1 = eval(p, op-1);
-		val2 = eval(op+1, q);
-
+		//val1 = eval(p, op-1);
+		//val2 = eval(op+1, q);
+		/*
 		switch(token_type) {
 			case '+': return val1 + val2;
-			case '-': return /* */
+			case '-': ;
 			case '*': ;
 			case '/': ;
 			default: assert(0);
-		}
+			;
+		}*/
 	}
-}
-				
-			
-uint32_t expr(char *e, bool *success) {
-	if(!make_token(e)) {
-		*success = false;
-		return 0;
+
+	return 0;
 }
 
+/*
+ * @describe returns the characters in a string beginning at the specified
+ *		location through the specified number of characters.
+ * @param {string, int, int} ch, pos, length
+ * @return {string}
+ */
+char *substring(char* ch, int pos, int length) {
+	char *pch = ch;
+
+	char *subch = (char *)calloc(sizeof(char), length+1);
+
+	int i;
+
+	pch = pch + pos;
+
+	for(i = 0; i < length; i++) {
+		subch[i] = *(pch++);
+	}
+	subch[length] = '\0';
+
+	return subch;
+}
+
+uint32_t expr(char *e, bool *success) {
+    int i;
+	if(!make_token(e)) {
+		*success = false;
+		printf("Error");
+		return 0;
+	}
 	/* TODO: Insert codes to evaluate the expression. */
-	panic("please implement me");
+	for(i = 0; i < nr_token; i++) {
+		if(tokens[i].type == '*' && (i == 0 || tokens[i-1].type != DEC_NUM)) {
+			//tokens[i].type == DEREF;
+			;
+		}
+	}
+	for(i = 0; i < nr_token; i++) {
+		printf("%s\n", tokens[i].str);
+	}
+	//panic("please implement me");
 	return 0;
 }
