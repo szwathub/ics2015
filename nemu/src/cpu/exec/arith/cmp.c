26495a66d373ad76d8d195aca297138390b7361c
szwathub <szwathub@gmail.com>
2015-11-03 16:49:53 +0800

    Implement some cmp instruction

--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -11,3 +11,5 @@
 #define DATA_BYTE 4
 #include "cmp-template.h"
 #undef DATA_BYTE
+
+make_helper_v(cmp_si2rm)
--- nemu/src/cpu/exec/arith/cmp.h
+++ nemu/src/cpu/exec/arith/cmp.h
@@ -1,5 +1,5 @@
 #ifndef __CMP_H__
 #define __CMP_H__
 
-
+make_helper(cmp_si2rm_v);
 #endif
