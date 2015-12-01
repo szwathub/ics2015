297d78564001442252a2c27386f74d974b4e43d4
szwathub <szwathub@gmail.com>
2015-12-01 14:32:27 +0800

    rewrite instruction push

--- nemu/src/cpu/exec/data-mov/push-template.h
+++ nemu/src/cpu/exec/data-mov/push-template.h
@@ -3,9 +3,16 @@
 #define instr push
 
 static void do_execute() {
-	cpu.esp -= DATA_BYTE;
-	swaddr_write(cpu.esp, DATA_BYTE, op_src->val);
-	print_asm_template1();
+    if(DATA_BYTE == 2){
+        cpu.esp -= 2;
+        MEM_W(cpu.esp, op_src->val);
+    }
+    else {
+        cpu.esp -= 4;
+        MEM_W(cpu.esp, op_src->val);
+    }
+    
+    print_asm_template1();
 }
 
 make_instr_helper(r)
