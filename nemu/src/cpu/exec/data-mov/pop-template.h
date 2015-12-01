89fe81a90159d83d64fca7db70bb8997f8fb7158
szwathub <szwathub@gmail.com>
2015-12-01 20:34:20 +0800

    Implement instruction pop

---
+++ nemu/src/cpu/exec/data-mov/pop-template.h
@@ -0,0 +1,16 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute() {
+    OPERAND_W(op_src, MEM_R(cpu.esp));
+    cpu.esp += DATA_BYTE;
+
+    print_asm_template1();
+}
+
+make_instr_helper(r);
+make_instr_helper(rm);
+
+
+#include "cpu/exec/template-end.h"
