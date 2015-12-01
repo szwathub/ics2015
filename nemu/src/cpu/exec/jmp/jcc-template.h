57bde254f1193c018bcc9af1826eaf2122d1c8e7
szwathub <szwathub@gmail.com>
2015-12-01 14:45:44 +0800

    test program add
    implement instruction cmp and some instruction in jcc

--- nemu/src/cpu/exec/jmp/jcc-template.h
+++ nemu/src/cpu/exec/jmp/jcc-template.h
@@ -5,7 +5,6 @@ static void do_execute() {
     if(cpu.ZF == 1) {
         cpu.eip += op_src->val;
     }
-
     print_asm_template1();
 }
 make_instr_helper(i)
@@ -15,12 +14,33 @@ make_instr_helper(i)
 static void do_execute() {
     if(cpu.CF == 1 || cpu.ZF == 1) {
         cpu.eip += op_src->val;
-        /* HACK for instruction jnbe_i_b */
-        /* TODO: fix the magic number */
-        cpu.eip &= 0xFFFFF0FF;
+        /* HACK: */
+        cpu.eip &= 0xffff0ff;
     }
 }
 make_instr_helper(i)
 #undef instr
+
+#define instr jno
+static void do_execute() {
+    if(cpu.OF == 0) {
+        cpu.eip += op_src->val;
+    }
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jle
+static void do_execute() {
+    printf("%x\n", cpu.eip);
+    printf("%x\n", op_src->val);
+    if(cpu.ZF == 1 || cpu.SF != cpu.OF){
+        cpu.eip += op_src->val;
+    }
+    printf("%x\n", cpu.eip);
+}
+make_instr_helper(i)
+#undef instr
+
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/jmp/jcc.h
+++ nemu/src/cpu/exec/jmp/jcc.h
@@ -3,6 +3,12 @@
 
 make_helper(jz_i_b);
 make_helper(jbe_i_b);
+make_helper(jno_i_b);
+make_helper(jle_i_b);
+
+make_helper(jno_i_v);
+make_helper(jle_i_v);
+
 /*
 make_helper(ja_i_b);
 make_helper(jae_i_b);
@@ -14,7 +20,7 @@ make_helper(jecxz_i_b);
 make_helper(jg_i_b);
 make_helper(jge_i_b);
 make_helper(jl_i_b);
-make_helper(jle_i_b);
+
 make_helper(jna_i_b);
 make_helper(jnae_i_b);
 make_helper(jnb_i_b);
@@ -25,7 +31,7 @@ make_helper(jng_i_b);
 make_helper(jnge_i_b);
 make_helper(jnl_i_b);
 make_helper(jnle_i_b);
-make_helper(jno_i_b);
+
 make_helper(jnp_i_b);
 make_helper(jns_i_b);
 make_helper(jnz_i_b);
@@ -46,7 +52,7 @@ make_helper(jz_i_v);
 make_helper(jg_i_v);
 make_helper(jge_i_v);
 make_helper(jl_i_v);
-make_helper(jle_i_v);
+
 make_helper(jna_i_v);
 make_helper(jnae_i_v);
 make_helper(jnb_i_v);
@@ -58,7 +64,7 @@ make_helper(jnge_i_v);
 make_helper(jnl_i_v);
 make_helper(jnle_i_v);
 make_helper(jno_i_v);
-make_helper(jnp_i_v);
+
 make_helper(jns_i_v);
 make_helper(jnz_i_v);
 make_helper(jo_i_v);
