3e3de2e756e32a0f3dbdef7d05eacd6dfaad170e
szwathub <szwathub@gmail.com>
2015-12-01 14:26:18 +0800

    reimplement instruction call

--- nemu/src/cpu/exec/jmp/call-template.h
+++ nemu/src/cpu/exec/jmp/call-template.h
@@ -1,16 +1,37 @@
 #include "cpu/exec/template-start.h"
 
-#define instr call
+make_helper(concat(call_i_, SUFFIX)){
+    int len = concat(decode_i_, SUFFIX)(eip + 1);
+    if(DATA_BYTE == 2) {
+        cpu.esp -= 2;
+        MEM_W(cpu.esp, cpu.eip + len + 1);
+        cpu.eip = (cpu.eip + op_src->val) & 0x0000ffff;
+    }
 
-static void do_execute() {
-    cpu.esp -= 4;
-    swaddr_write(cpu.esp, 4, cpu.eip);
-    cpu.eip += op_src->val;
-
+    if(DATA_BYTE == 4){
+        cpu.esp -= 4;
+        MEM_W(cpu.esp,  cpu.eip + len + 1);
+        cpu.eip += op_src->val;
+    }
     print_asm_template1();
+
+    return len + 1;
 }
 
-make_instr_helper(i)
-make_instr_helper(rm)
+make_helper(concat(call_rm_, SUFFIX)){
+    int len = concat(decode_rm_, SUFFIX)(eip + 1);
+    if(2 == DATA_BYTE){
+        cpu.esp -= 2;
+        MEM_W(cpu.esp, cpu.eip + len + 1);
+        cpu.eip = op_src->val & 0x0000ffff;
+    }
+    else if(4 == DATA_BYTE){
+        cpu.esp -= 4;
+        MEM_W(cpu.esp, cpu.eip + len + 1);
+        cpu.eip = op_src->val;
+    }
+    print_asm_template1();
 
+    return 0;
+}
 #include "cpu/exec/template-end.h"
